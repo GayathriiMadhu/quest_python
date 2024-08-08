97de6048c4221ae8e8fb98c99c16fdc71c7338e5
GayathriiMadhu <gayathri.madhung@gmail.com>
2024-08-09 00:59:48 +0530

    updated

--- Week2/day8/admission.py
+++ Week2/day8/admission.py
@@ -24,3 +24,28 @@
 # listMarks = i.split(',')
 # print(type(listMarks))
 # print(len(listMarks))
+def check_eligibility(branch, maths, arts, literature, preference1, preference2):
+    eligible_for = []
+    if branch == "ec" and arts > 90 and literature > 90 and ((preference1 or preference2)=="arts" or "literature"):
+        eligible_for.append("marketing")
+    if branch == "bcom" and maths > 95 and ((preference1 or preference2)=="maths"):
+        eligible_for.append("accountancy")
+    if branch == "cs" and maths > 90 and literature > 90 and ((preference1 or preference2)=="maths" or "literature"):
+        eligible_for.append("sales")
+    return eligible_for
+branch = input("Enter your branch (ec, bcom, cs): ").lower()
+
+maths = int(input("Enter your maths marks: "))
+arts = int(input("Enter your arts marks: "))
+literature = int(input("Enter your literature marks: "))
+if maths <= 35 or arts <= 35 or literature <= 35:
+    print("You are not eligible for any course due to low marks")
+
+else:
+    preference1 = input("Enter your 1st preference (maths, arts, literature): ").lower()
+    preference2 = input("Enter your 2nd preference (maths, arts, literature): ").lower()
+    eligible_courses = check_eligibility(branch, maths, arts, literature, preference1, preference2)
+    if eligible_courses:
+        print("You are eligible for:", ", ".join(eligible_courses))
+    else:
+        print("You are not eligible for any course")
